551f7f5f3967efeadd4b74ab2e27e4008f68a905
alibad01 <alioune.diallo@clavus.io>
2024-10-07 15:54:57 +0000

    Update project files 2

--- src/app/api/users/find/route.ts
+++ src/app/api/users/find/route.ts
@@ -1,7 +1,9 @@
+import connect from "@/lib/db";
 import User from "@/models/user.model";
 import { NextResponse } from "next/server";
 
 export async function GET() {
+  await connect();
   const users = await User.find();
   return NextResponse.json({ users });
 }
