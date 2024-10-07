d157f0fa7832a216e00dac3f81c7729d97de225f
alibad01 <alioune.diallo@clavus.io>
2024-10-07 15:54:57 +0000

    Update project files 2

--- src/app/api/users/findOne/[id]/route.ts
+++ src/app/api/users/findOne/[id]/route.ts
@@ -1,7 +1,9 @@
+import connect from "@/lib/db";
 import User from "@/models/user.model";
 import { NextResponse } from "next/server";
 
 export async function GET(request: Request) {
+  await connect();
   const { searchParams } = new URL(request.url);
   const id = searchParams.get('id');
   const user = await User.findById(id);
